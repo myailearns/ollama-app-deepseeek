6b88a556c1b81d00d5bd9049e900e712fe405232
myailearns <myailearns@gmail.com>
2025-08-14 12:22:00 +0530

    changes

--- deepsek_demo.py
+++ deepsek_demo.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.title("Ask anything")
 
 
-llm = ChatOllama(model="deepseek-r1:8b")
+llm = ChatOllama(model="deepseek-r1:8b",base_url="https://5753708059b4.ngrok-free.app")
 
 question = st.text_input("Enter query")
 
